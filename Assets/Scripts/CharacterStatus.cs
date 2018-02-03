5d96f6a8a61002e86ba1eb90d898c5038eccf0ab
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-02-03 11:41:38 +0100

    Refactored CharacterController and Status

---
+++ Assets/Scripts/CharacterStatus.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+
+public class CharacterStatus : MonoBehaviour
+{
+    private bool _isRunning = false;
+    private bool _isAttacking = false;
+
+    private Animator myAnimator;
+
+    // Use this for initialization
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    public bool RunningStatus
+    {
+        get { return _isRunning; }
+        set
+        {
+            _isRunning = value;
+            myAnimator.SetBool("isRunning", _isRunning);
+        }
+    }
+
+    public bool AttackingStatus
+    {
+        get { return _isAttacking; }
+        set
+        {
+            _isAttacking = value;
+            myAnimator.SetBool("isAttacking", _isAttacking);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Fighter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    [SerializeField]
+    private int health = 1;
+
+    // Use this for initialization
+    void Start ()
+    {
+    		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Hit (int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+            Die();
+    }
+
+    private void Die()
+    {
+
+    }
+}
