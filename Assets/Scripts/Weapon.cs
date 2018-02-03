5804892cf287d2dc95a3ca0923d7e065b4d27858
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-02-03 19:09:55 +0100

    Attack animation wip

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,12 +7,16 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     [SerializeField]
+    private CharacterStatus weaponHolder;
+
+    [SerializeField]
     private int damage = 1;
 
     // Use this for initialization
     void Start ()
     {
-		
+        if (weaponHolder == null)
+            Debug.LogError("This weapon has no holder.");
 	}
 	
 	// Update is called once per frame
@@ -23,9 +27,12 @@ public class Weapon : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        Fighter hitEnemy = collision.collider.GetComponent<Fighter>();
-        if (hitEnemy)
-            hitEnemy.Hit(damage);
-
+        if (weaponHolder.AttackingStatus)
+        {
+            Debug.Log("Hit");
+            Hittable hitTarget = collision.collider.GetComponent<Hittable>();
+            if (hitTarget)
+                hitTarget.Hit(damage);
+        }
     }
 }
