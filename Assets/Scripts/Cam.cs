e609ce9bf30285c481fa8d0c41d559a23818771d
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-02-20 12:13:02 +0100

    Camera wall detection wip

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -14,12 +14,15 @@ public class Cam : MonoBehaviour
     private float x = 0.0f;
     private float y = .0f;
     public bool zoom;
+    private float standardDistance;
+    public LayerMask wallLayerMask;
 
     void Start()
     {
         Vector3 angles = transform.eulerAngles;
         x = angles.y;
         y = angles.x;
+        standardDistance = distance;
     }
 
     void LateUpdate()
@@ -47,10 +50,28 @@ public class Cam : MonoBehaviour
             Quaternion rotation = Quaternion.Euler(y, x, 0);
             Vector3 position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.position;
 
+            //Check walls
+            RaycastHit hitInfo;
+            Debug.DrawRay(position, target.position - position, Color.green);
+            if(Physics.Raycast(target.position, (position - target.position).normalized, out hitInfo, Vector3.Distance(target.position, position), wallLayerMask))
+            { 
+                //Debug.DrawRay(target.position, position - target.position, Color.red);   
+                position = hitInfo.point;
+                distance = Vector3.Distance(target.position, hitInfo.point);
+                Debug.DrawLine(hitInfo.point + transform.right, hitInfo.point - transform.right, Color.cyan);
+                Debug.DrawLine(hitInfo.point + transform.up, hitInfo.point - transform.up, Color.cyan);
+            }
+            else
+                distance = Mathf.Lerp(distance, standardDistance, Time.deltaTime * 2);
+
             transform.rotation = rotation;
             transform.position = position;
+
         }
     }
+
+
+
     private int ClampAngle(float angle, float min, float max)
     {
         Debug.Log(angle);
