9f4725bd864460971f02851e9a0724d1fff0b8f9
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-02-27 14:18:07 +0100

    Cam code cleaner

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -59,7 +59,6 @@ public class Cam : MonoBehaviour
         Vector3 position;
         transform.rotation = rotation;
 
-        //Check walls
         RaycastHit hitInfo;
         Vector3 cameraFocusPoint = GetCameraFocusPoint();
         float distanceBetween = Vector3.Distance(defaultTarget.position, target.position);//0 if target is the player
@@ -68,35 +67,23 @@ public class Cam : MonoBehaviour
 
         Debug.DrawRay(defaultTarget.position, idealCameraPos - cameraFocusPoint, Color.green);
 
+        //Check walls
         if (Physics.Raycast(defaultTarget.position, (idealCameraPos - cameraFocusPoint).normalized,
             out hitInfo, Vector3.Distance(defaultTarget.position, idealCameraPos), wallLayerMask))
         {
 
             if (Physics.Raycast(cameraFocusPoint, (idealCameraPos - cameraFocusPoint).normalized,
                 out hitInfo, Vector3.Distance(defaultTarget.position, idealCameraPos), wallLayerMask))
-            //Debug.DrawRay(target.position, position - target.position, Color.red); 
-            {
-                //if (Vector3.Distance(transform.position, hitInfo.point) < 0.1f)
-                    position = hitInfo.point;
-                //else
-                //    position = Vector3.Lerp(transform.position, hitInfo.point, Time.deltaTime * 10);
-            }
+                position = hitInfo.point;//We want the point hit by the cameraFocus, not the character
             else
-            {
-                //if (Vector3.Distance(transform.position, idealCameraPos) < 0.1f)
-                    position = idealCameraPos;
-                //else
-                //    position = Vector3.Lerp(transform.position, idealCameraPos, Time.deltaTime * 10);
-            }
+                position = idealCameraPos;
 
             distance = Vector3.Distance(cameraFocusPoint, hitInfo.point);
-            Debug.DrawLine(hitInfo.point + transform.right, hitInfo.point - transform.right, Color.cyan);
-            Debug.DrawLine(hitInfo.point + transform.up, hitInfo.point - transform.up, Color.cyan);
+            //Debug.DrawLine(hitInfo.point + transform.right, hitInfo.point - transform.right, Color.cyan);
+            //Debug.DrawLine(hitInfo.point + transform.up, hitInfo.point - transform.up, Color.cyan);
         }
-        else// if (!target.Equals(defaultTarget))
+        else
             position = idealCameraPos;
-        //else
-        //    position = Vector3.Lerp(transform.position, idealCameraPos, Time.deltaTime * 10);
 
         transform.position = position;
     }
@@ -119,16 +106,10 @@ public class Cam : MonoBehaviour
         {
             Transform lockTarget = defaultTargetManager.GetNearestTarget();
             if (lockTarget != null)
-            {
-                //distance += Vector3.Distance(defaultTarget.position, lockTarget.position);
                 target = lockTarget;
-            }
         }
         else if (IsTargetReleased())
-        {
             target = defaultTarget;
-            //distance = standardDistance;
-        }
 
     }
 
@@ -152,74 +133,6 @@ public class Cam : MonoBehaviour
         UpdateTarget();
         GetCameraInput();
         UpdatePosition();
-        /*if (target)
-        {
-
-            if (IsTriggerDown() && !IsTargetInsideView())
-            {
-                //distance = Vector3.Distance(target.position, transform.position);
-                //distance = Mathf.Lerp(distance, distance + 1f, Time.deltaTime * 50);
-                distance += 0.1f;
-                Debug.Log(target.gameObject.name);
-            }
-            else if(IsTargetInsideView() && !IsTriggerDown())
-            {          
-                distance = standardDistance;
-                //Debug.Log("A");
-                Debug.Log(target.gameObject.name);
-            }
-
-
-            Vector3 position = target.position + rotation * new Vector3(0.0f, 0.0f, -distance);
-
-
-            /*
-            //Check walls
-            RaycastHit hitInfo;
-            Debug.DrawRay(position, target.position - position, Color.green);
-            if (Physics.Raycast(target.position, (position - target.position).normalized, out hitInfo, Vector3.Distance(target.position, position), wallLayerMask))
-            {
-                //Debug.DrawRay(target.position, position - target.position, Color.red);   
-                position = hitInfo.point;
-                distance = Vector3.Distance(target.position, hitInfo.point);
-                Debug.DrawLine(hitInfo.point + transform.right, hitInfo.point - transform.right, Color.cyan);
-                Debug.DrawLine(hitInfo.point + transform.up, hitInfo.point - transform.up, Color.cyan);
-            }
-            else
-            {
-                //if (!lerping)
-                //{
-                //    lerping = true;
-                //StartCoroutine(LerpBackToNormal(distance));
-                Vector3 screenPoint = GetComponent<Camera>().WorldToViewportPoint(defaultTarget.position);
-                /*if (!(screenPoint.z > -0.2f && screenPoint.x > -0.2f && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1))
-                {
-                    distance = Mathf.Lerp(distance, distance + 1f, Time.deltaTime * 10);
-                    Debug.Log("A");
-                }
-                else
-                {
-                //float triggerPressed = Input.GetAxisRaw("TargetLock");
-
-                //if (triggerPressed < 0.9f)
-                //{
-
-                if (Mathf.Abs(distance - standardDistance) > 0.1f)
-                {
-                    Debug.Log("B");
-                    distance = Mathf.Lerp(distance, standardDistance, Time.deltaTime * 2);
-                }
-                else
-                {
-                    Debug.Log("C");
-                    distance = standardDistance;
-                }
-                
-            }
-          
-            transform.rotation = rotation;
-            transform.position = position;
-        }*/
     }
 
     bool IsTriggerDown()
@@ -231,19 +144,6 @@ public class Cam : MonoBehaviour
     bool IsTriggerToggled()
     {
         return Input.GetButtonDown("TargetLock");
-    }
-
-    IEnumerator LerpBackToNormal(float initialDistance)
-    {
-        while (Mathf.Abs(distance - standardDistance) > 0.01f)
-        {
-            distance = Mathf.Lerp(initialDistance, standardDistance, Time.deltaTime * 10);
-            Debug.Log("lerping");
-            yield return null;
-        }
-        distance = standardDistance;
-        lerping = false;
-        Debug.Log("LerpDone");
     }
 
     private int ClampAngle(float angle, float min, float max)
