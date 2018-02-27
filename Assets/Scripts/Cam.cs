2b5a6bda90679d779aead527198d0af3b4fa0fb4
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-02-27 10:50:20 +0100

    Camera lock wip

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
 
 public class Cam : MonoBehaviour
 {
     public Transform target;
+    private Transform defaultTarget;
+
+    private TargetManager defaultTargetManager;
+
     public float distance = 10.0f;
     public float xSpeed = 250.0f;
     public float ySpeed = 120.0f;
@@ -13,44 +17,114 @@ public class Cam : MonoBehaviour
     public float zoomSpeed = 120.0f;
     private float x = 0.0f;
     private float y = .0f;
-    public bool zoom;
+    public float lockDistance = 8f;
     private float standardDistance;
     public LayerMask wallLayerMask;
     private bool lerping;
+    [SerializeField] private float lockCamMinDistance = 3f;
+    [SerializeField] private float maxTargetDistance = 10f;
 
     void Start()
     {
         Vector3 angles = transform.eulerAngles;
+        defaultTarget = target;
+        defaultTargetManager = target.GetComponent<TargetManager>();
         x = angles.y;
         y = angles.x;
         standardDistance = distance;
     }
 
+    private void GetCameraInput()
+    {
+        float horizontal = Input.GetAxis("RightHorizontal");
+        float vertical = Input.GetAxis("RightVertical");
+
+        if (Mathf.Abs(horizontal) > 0.5f)
+            x += (float)(horizontal * xSpeed * 0.02);
+
+        if (Mathf.Abs(vertical) > 0.5f)
+            y += (float)(vertical * zoomSpeed * 0.02);
+
+        y = ClampAngle(y, yMinLimit, yMaxLimit);
+    }
+
+    bool IsRightStickPressed()
+    {
+        return true;
+    }
+
+    void UpdatePosition()
+    {
+        Quaternion rotation = Quaternion.Euler(y, x, 0);
+        Vector3 position;
+        if (target.Equals(defaultTarget))
+            position = target.position + rotation * new Vector3(0.0f, 0.0f, -distance);
+        else
+        {
+            Vector3 middlePoint = new Vector3((defaultTarget.position.x + target.position.x) * 0.5f, (defaultTarget.position.y + target.position.y) * 0.5f,
+                                                                                                     (defaultTarget.position.z + target.position.z) * 0.5f);
+            float distanceBetween = Vector3.Distance(defaultTarget.position, target.position);
+            distance = distanceBetween + lockCamMinDistance;
+            position = middlePoint + rotation * new Vector3(0.0f, 0.0f, -distance);
+        }
+        transform.rotation = rotation;
+        transform.position = position;
+    }
+
+    void UpdateTarget()
+    {
+        if (Input.GetButtonDown("TargetLock"))
+        {
+            Transform lockTarget = defaultTargetManager.GetNearestTarget();
+            if (lockTarget != null)
+            {
+                distance += Vector3.Distance(defaultTarget.position, lockTarget.position);
+                target = lockTarget;
+            }
+        }
+        else if (IsTargetReleased())// || Vector3.Distance(target, defaultTarget) > max)
+        {
+            target = defaultTarget;
+            distance = standardDistance;
+        }
+
+    }
+
+    bool IsTargetReleased()
+    {
+        if (target.Equals(defaultTarget))
+            return true;
+        else
+            return (Input.GetButtonUp("TargetLock") || Vector3.Distance(target.position, defaultTarget.position) > maxTargetDistance);
+    }
+
     void LateUpdate()
     {
-        if (target)
+        UpdateTarget();
+        GetCameraInput();
+        UpdatePosition();
+        /*if (target)
         {
-            float horizontal = Input.GetAxis("RightHorizontal");
-            float vertical = Input.GetAxis("RightVertical");
 
-            if (Mathf.Abs(horizontal) > 0.5f)
-                x += (float)(horizontal * xSpeed * 0.02);
-
-            if (zoom)
+            if (IsTriggerDown() && !IsTargetInsideView())
             {
-                distance -= (float)(vertical);
+                //distance = Vector3.Distance(target.position, transform.position);
+                //distance = Mathf.Lerp(distance, distance + 1f, Time.deltaTime * 50);
+                distance += 0.1f;
+                Debug.Log(target.gameObject.name);
             }
-            else
-            {
-                if (Mathf.Abs(vertical) > 0.5f)
-                    y += (float)(vertical * zoomSpeed * 0.02);
+            else if(IsTargetInsideView() && !IsTriggerDown())
+            {          
+                distance = standardDistance;
+                //Debug.Log("A");
+                Debug.Log(target.gameObject.name);
             }
 
-            y = ClampAngle(y, yMinLimit, yMaxLimit);
 
-            Quaternion rotation = Quaternion.Euler(y, x, 0);
-            Vector3 position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.position;
+            Vector3 position = target.position + rotation * new Vector3(0.0f, 0.0f, -distance);
 
+
+            /*
             //Check walls
             RaycastHit hitInfo;
             Debug.DrawRay(position, target.position - position, Color.green);
@@ -68,32 +142,64 @@ public class Cam : MonoBehaviour
                 //{
                 //    lerping = true;
                 //StartCoroutine(LerpBackToNormal(distance));
-                if (Mathf.Abs(distance - standardDistance) > 0.1f)
-                    distance = Mathf.Lerp(distance, standardDistance, Time.deltaTime * 2);
+                Vector3 screenPoint = GetComponent<Camera>().WorldToViewportPoint(defaultTarget.position);
+                /*if (!(screenPoint.z > -0.2f && screenPoint.x > -0.2f && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1))
+                {
+                    distance = Mathf.Lerp(distance, distance + 1f, Time.deltaTime * 10);
+                    Debug.Log("A");
+                }
                 else
+                {
+                //float triggerPressed = Input.GetAxisRaw("TargetLock");
+
+                //if (triggerPressed < 0.9f)
+                //{
+
+                if (Mathf.Abs(distance - standardDistance) > 0.1f)
+                {
+                    Debug.Log("B");
+                    distance = Mathf.Lerp(distance, standardDistance, Time.deltaTime * 2);
+                }
+                else
+                {
+                    Debug.Log("C");
                     distance = standardDistance;
-                //}
+                }
+                
             }
+          
             transform.rotation = rotation;
             transform.position = position;
-        }
+        }*/
+    }
+
+    bool IsTriggerDown()
+    {
+        return Input.GetButton("TargetLock");
+    }
+
+
+    bool IsTriggerToggled()
+    {
+        return Input.GetButtonDown("TargetLock");
     }
 
     IEnumerator LerpBackToNormal(float initialDistance)
     {
-        while(Mathf.Abs(distance - standardDistance) > 0.2f)
+        while(Mathf.Abs(distance - standardDistance) > 0.01f)
         {
-            distance = Mathf.Lerp(initialDistance, standardDistance, Time.deltaTime * 2);
+            distance = Mathf.Lerp(initialDistance, standardDistance, Time.deltaTime * 10);
+            Debug.Log("lerping");
             yield return null;
         }
         distance = standardDistance;
         lerping = false;
+        Debug.Log("LerpDone");
     }
-
 
     private int ClampAngle(float angle, float min, float max)
     {
-        Debug.Log(angle);
+        //Debug.Log(angle);
 
         if (angle < -360)
         {
---
+++ Assets/TargetManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+    private HashSet<Transform> nearTargets;
+
+    void Start()
+    {
+        nearTargets = new HashSet<Transform>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.GetComponent<Target>() != null)
+        {
+            nearTargets.Add(collider.gameObject.transform);
+            Debug.Log("IN");
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.GetComponent<Target>() != null)
+        {
+            nearTargets.Remove(collider.gameObject.transform);
+            Debug.Log("OUT");
+        }
+    }
+
+    public Transform GetNearestTarget()
+    {
+        float minDist = Mathf.Infinity;
+        Transform nearestTarget = null;
+
+        float currDist;
+        foreach (Transform t in nearTargets)
+        {
+            currDist = Vector3.Distance(t.position, transform.position);
+            if (currDist < minDist)
+            {
+                minDist = currDist;
+                nearestTarget = t;
+            }
+        }
+
+        return nearestTarget;
+    }
+}
