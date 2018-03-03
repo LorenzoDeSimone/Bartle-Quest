7b8ad2f97a8c805a5ec041d90d276f8133360f6a
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-03-03 21:02:40 +0100

    First working fsm framework

--- Assets/Scripts/Finite State Machines/State.cs
+++ Assets/Scripts/Finite State Machines/State.cs
@@ -35,9 +35,7 @@ public class State : ScriptableObject
                 controller.TransitionToState(transitions[i].trueState);
             }
             else
-            {
                 controller.TransitionToState(transitions[i].falseState);
-            }
         }
     }
 
