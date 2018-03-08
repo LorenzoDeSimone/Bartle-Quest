884ce22a0d0c912f73c73252b5df67ac966aff39
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-03-08 22:49:17 +0100

    Mechanim combo system

--- Assets/Scripts/CharacterStatus.cs
+++ Assets/Scripts/CharacterStatus.cs
@@ -13,6 +13,8 @@ public class CharacterStatus : MonoBehaviour
     private Animator myAnimator;
 
     private bool shieldUpThisFrame;
+    private float currAnimationLenght;
+    private int lastAnimatorState;
 
     // Use this for initialization
     void Start()
@@ -51,6 +53,9 @@ public class CharacterStatus : MonoBehaviour
         //Checks if the character is in one of the states labelled as attackStates in the animator
         get
         {
+            if (myAnimator.GetAnimatorTransitionInfo(0).IsName("AnyState -> " + AttackStates[0]))
+                return true;
+
             foreach (string attackState in AttackStates)
             {
                 if (myAnimator.GetCurrentAnimatorStateInfo(0).IsName(attackState))
@@ -69,21 +74,59 @@ public class CharacterStatus : MonoBehaviour
 
     public void RequestAttack()
     {
-        //The character is not attacking and combo needs to start
-        if (!AttackingStatus && !myAnimator.GetAnimatorTransitionInfo(0).IsName("AnyState -> "+ AttackStates[0]))
+        AnimatorStateInfo currentAnimatorState = myAnimator.GetCurrentAnimatorStateInfo(0);
+        //if (AttackingStatus  && currentAnimatorState.fullPathHash != lastAnimatorState)
+
+            //The character is not attacking and combo needs to start
+        if (!AttackingStatus)
         {
             //Debug.Log(AttackStates[0] + "FIRST PRESS");
+            //StartCoroutine(ReturnToIdle());
             myAnimator.SetTrigger("firstAttack");
+            //currentAnimatorState = myAnimator.GetCurrentAnimatorStateInfo(0);
+            //lastAnimatorState = currentAnimatorState.fullPathHash;
         }
         //If character is already attacking, input is registered to act when current animation is finished
         //If the button is pressed during last attack state, trigger combo should not be set
-        else if(myAnimator.GetCurrentAnimatorStateInfo(0).IsName(AttackStates[AttackStates.Length-1]))
+        /*else if(myAnimator.GetCurrentAnimatorStateInfo(0).IsName(AttackStates[AttackStates.Length-1]))
         {
             myAnimator.ResetTrigger("comboButtonPressed");
-        }
-        else{
+        }*/
+        else if (AttackingStatus)
+        {
+            //lastAnimatorState = currentAnimatorState.fullPathHash;
             myAnimator.SetTrigger("comboButtonPressed");
+            //currAnimationLenght = myAnimator.GetCurrentAnimatorStateInfo(0).length;
             //Debug.Log("COMBO PRESS");
         }
+    }
+
+    /*
+    void Update()
+    {
+        if(AttackingStatus && myAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.25f)
+            myAnimator.SetBool("comboButtonPressed", false);
+
+        AnimatorStateInfo currentAnimatorState = myAnimator.GetCurrentAnimatorStateInfo(0);
+        if (currentAnimatorState.fullPathHash != lastAnimatorState)
+            myAnimator.SetBool("comboButtonPressed", false);
+
+    }*/
+
+    private IEnumerator ReturnToIdle()
+    {
+        while(true)
+        {
+            //Debug.Log(myAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+            if (AttackingStatus && myAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.8f)
+                myAnimator.SetTrigger("returnToIdle");
+            yield return null;
+        }
+    }
+
+    public void DisableTransitionsToFalse()
+    {
+        myAnimator.SetBool("disableTransitions", false);
+        Debug.Log("q");
     }
 }
