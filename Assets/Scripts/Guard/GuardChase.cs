a21a4c2f48669c370d1ef22a94211744f8173144
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-03-10 21:55:05 +0100

    Navmesh agents rotation tweaks

--- Assets/Scripts/Guard/GuardChase.cs
+++ Assets/Scripts/Guard/GuardChase.cs
@@ -15,7 +15,7 @@ public class GuardChase : GuardState
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {        
-        RotateTowards(myGuardStatus.target.position);
+        //RotateTowards(myGuardStatus.target.position);
         navMeshAgent.destination = myGuardStatus.target.position;
         navMeshAgent.isStopped = false;
 
