10f62998fb906ba8a8637c5c9cbd8734bf7131eb
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-03-11 02:31:16 +0100

    Guard cool Death Effect

--- Assets/Scripts/Guard/GuardDeath.cs
+++ Assets/Scripts/Guard/GuardDeath.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class GuardDeath : GuardState
 {
     private float elapsedTime;
-    private Material myMaterial;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         Initialization(animator);
+        navMeshAgent.isStopped = true;
     }
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         elapsedTime += Time.deltaTime;
 
-        if(elapsedTime >= myGuardStatus.timeToDisappearAfterDeath)
+        if (elapsedTime >= myGuardStatus.timeToDisappearAfterDeath)
+        {
+            myGuardStatus.DeathFade.SetActive(true);
+            myGuardStatus.DeathFade.transform.parent = null;
             Destroy(myGuardStatus.gameObject);
+        }
         //gameObject.GetComponent<MeshRenderer>().material = myMaterial;
     }
 
