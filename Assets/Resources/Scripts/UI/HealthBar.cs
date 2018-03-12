5ee4e6065cfc8acdebc500b9a45478e1e651c06e
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-03-12 18:48:56 +0100

    Reordering folder. Enemy Health bar working

---
+++ Assets/Resources/Scripts/UI/HealthBar.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Transform characterToFollow;
+    public Hittable characterHittable;
+    public Image healthBar;
+    public float height = 2f;
+    private static Vector3 Scale = new Vector3(1.8f, 0.8f, 1f);
+
+
+    public static void CreateHealthBar(Transform objectToFollow, Hittable hittable)
+    {
+        GameObject Canvas = GameObject.Find("Canvas");
+
+        UnityEngine.Object healthBarPrefab = Resources.Load("Prefabs/UI/HealthBar");
+        GameObject healthBarGO = (GameObject) Instantiate(healthBarPrefab);
+
+        HealthBar myHealthBar;
+        myHealthBar = healthBarGO.GetComponent<HealthBar>();
+        myHealthBar.characterHittable = hittable;
+        myHealthBar.characterToFollow = objectToFollow;
+
+        myHealthBar.transform.SetParent(Canvas.transform);
+        myHealthBar.transform.localPosition = Vector3.zero;
+        myHealthBar.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        myHealthBar.transform.localScale = Scale;
+    }
+
+    void Start()
+    {
+        if (!healthBar)
+            Debug.LogError("The health bar is missing bar image or background image.");
+    }
+
+    void LateUpdate()
+    {
+        if (characterToFollow)
+        {
+            transform.position = characterToFollow.transform.position + Vector3.up * height;
+            healthBar.fillAmount = (float)characterHittable.CurrentHealth / (float)characterHittable.MaxHealth;
+        }
+        else
+            Destroy(gameObject);
+    }
+}
