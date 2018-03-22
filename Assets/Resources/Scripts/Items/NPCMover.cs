6002eb3ee5eb8483a0e28861e1737b757ea953fc
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-03-22 15:25:24 +0100

    NPC mover lv1

---
+++ Assets/Resources/Scripts/Items/NPCMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMover : MonoBehaviour
+{
+    [SerializeField] private Transform NPC;
+    [SerializeField] private Transform NPCFinalTransform;
+    [SerializeField] private string dialogueName;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.GetComponent<PlayerController>())
+        {
+            NPC.position = NPCFinalTransform.position;
+            NPC.rotation = NPCFinalTransform.rotation;
+            NPC.GetComponent<Talker>().enabled = false;
+            NPC.GetComponent<Target>().enabled = false;
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/NPCs/DialogueTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [SerializeField] private DialogueManager dialogueManager;
+    [SerializeField] private Talker myTalker;
+    [SerializeField] private bool oneShot = true;
+    private bool dialogueFired = false;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.GetComponent<PlayerController>() && myTalker && !dialogueFired)
+        {
+            dialogueManager.InitDialogue(myTalker);
+            if (oneShot)
+                dialogueFired = true;
+        }
+    }
+}
