77c938377290d65d332a5aa124ffd00c1b21038a
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-03-30 00:30:43 +0200

    Level rating ui wip. Unity analytics added

---
+++ Assets/Resources/Scripts/DataSender.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class DataSender : MonoBehaviour
+{
+    public void SendData()
+    {
+        Dictionary<string, object> questEvaluationData = new Dictionary<string, object>();
+        questEvaluationData.Add("Quest Name", "Level1");
+        questEvaluationData.Add("Achiever"  , 0.25f);
+        questEvaluationData.Add("Explorer"  , 0f);
+        questEvaluationData.Add("Socializer", 0.25f);
+        questEvaluationData.Add("Killer"    , 0.5f);
+        questEvaluationData.Add("Reward"    , 0.8f);
+
+        Analytics.CustomEvent("Quest Evaluation", questEvaluationData);
+    }
+}
