2c2f8fdb25d35c6b3081bf7e68b99264ee90df6e
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-04-06 17:35:12 +0200

    lv 2 explorer path first prototype

---
+++ Assets/Resources/Scripts/Level2/GemSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSwitch : Interactable
+{
+   [SerializeField] private GameObject myWalls;
+    [SerializeField] private bool activeOnStart = true;
+
+    void Awake()
+    {
+        myWalls.SetActive(activeOnStart);
+    }
+
+    public override bool CanInteract()
+    {
+        return true;
+    }
+
+    public override void Interact()
+    {
+        myWalls.SetActive(!myWalls.active);
+    }
+}
