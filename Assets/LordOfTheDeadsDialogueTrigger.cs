92e120e42a010f9af13468d41cd47668cae75366
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-04-08 19:03:03 +0200

    lv2 boss logic start

---
+++ Assets/LordOfTheDeadsDialogueTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LordOfTheDeadsDialogueTrigger : MonoBehaviour
+{
+    [SerializeField] private DialogueManager dialogueManager;
+    [SerializeField] Transform barrier;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.GetComponent<PlayerController>())
+        {
+            if (PlayerChoices.Instance().BlessedSword)
+                GetComponent<Talker>().DialogueName = "LordOfTheDeadsExplorer";
+            else
+                GetComponent<Talker>().DialogueName = "LordOfTheDeadsAchiever";
+
+            dialogueManager.InitDialogue(GetComponent<Talker>());
+            barrier.gameObject.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
