27a1f653beb8ddf6a92515e9d9f16ced0261c8c9
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-04-18 01:28:02 +0200

    Level 3 wip. Basic colour riddle logic implemented

---
+++ Assets/Resources/Scenes/Level3/CandleColourChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleColourChanger : Interactable
+{
+    [SerializeField] PlayerCandle playerCandle;
+    [SerializeField] private PlayerCandle.CANDLE_COLOUR colour;
+
+    public override bool CanInteract()
+    {
+        return true;
+    }
+
+    public override void Interact()
+    {
+        playerCandle.AddColour(colour);
+    }
+}
