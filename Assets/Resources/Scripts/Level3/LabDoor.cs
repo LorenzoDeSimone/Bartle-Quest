c8a0344bfdf68399ab510f4e9519b869f7a57d18
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-04-18 12:18:08 +0200

    Added materials and finished lv3 riddle logic

---
+++ Assets/Resources/Scripts/Level3/LabDoor.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabDoor : MonoBehaviour
+{
+    [SerializeField] private LabDoorOrnament heart;
+    private PlayerCandle.CANDLE_COLOUR heartColour;
+
+    [SerializeField] private LabDoorOrnament stars;
+    private PlayerCandle.CANDLE_COLOUR starsColour;
+
+    [SerializeField] private LabDoorOrnament crown;
+    private PlayerCandle.CANDLE_COLOUR crownColour;
+
+    public Material redMaterial, yellowMaterial, blueMaterial, orangeMaterial, greenMaterial, purpleMaterial;
+    private Dictionary<PlayerCandle.CANDLE_COLOUR, Material> enumToMaterial;
+
+    [SerializeField] private PlayerCandle playerCandle;
+
+
+    void Start()
+    {
+        enumToMaterial = new Dictionary<PlayerCandle.CANDLE_COLOUR, Material>();
+        enumToMaterial.Add(PlayerCandle.CANDLE_COLOUR.BLUE  , blueMaterial);
+        enumToMaterial.Add(PlayerCandle.CANDLE_COLOUR.YELLOW, yellowMaterial);
+        enumToMaterial.Add(PlayerCandle.CANDLE_COLOUR.RED   , redMaterial);
+        enumToMaterial.Add(PlayerCandle.CANDLE_COLOUR.ORANGE, orangeMaterial);
+        enumToMaterial.Add(PlayerCandle.CANDLE_COLOUR.GREEN , greenMaterial);
+        enumToMaterial.Add(PlayerCandle.CANDLE_COLOUR.PURPLE, purpleMaterial);
+    }
+
+    public void ChangeMaterial(string ornamentName)
+    {
+        LabDoorOrnament ornament = GetOrnamentFromName(ornamentName);
+        MeshRenderer[] meshRenderers = ornament.GetComponentsInChildren<MeshRenderer>();
+        Material m;
+        if (enumToMaterial.TryGetValue(playerCandle.CurrentColour, out m))
+        {
+            foreach (MeshRenderer r in meshRenderers)
+                r.material = m;
+
+            ornament.currentColour = playerCandle.CurrentColour;
+            if (IsCombinationRight())
+                GetComponent<ExplodingDoor>().Explode();
+        }
+    }
+
+    private bool IsCombinationRight()
+    {
+        //I know...
+        return heart.currentColour.Equals(PlayerCandle.CANDLE_COLOUR.YELLOW) &&
+               stars.currentColour.Equals(PlayerCandle.CANDLE_COLOUR.BLUE)   &&
+               crown.currentColour.Equals(PlayerCandle.CANDLE_COLOUR.RED);
+    }
+
+    private LabDoorOrnament GetOrnamentFromName(string name)
+    {
+        if (name.Equals("heart"))
+            return heart;
+        else if (name.Equals("stars"))
+            return stars;
+        else if (name.Equals("crown"))
+            return crown;
+        else
+            return null;
+    }
+}
---
+++ Assets/Resources/Scripts/Level3/LabDoorOrnament.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabDoorOrnament : MonoBehaviour
+{
+    [SerializeField] public PlayerCandle.CANDLE_COLOUR currentColour;
+}
