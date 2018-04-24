f0200cbc0d566684600af704d7f13515525e1b46
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-04-24 17:26:22 +0200

    Level5 Killer path polishing

---
+++ Assets/LightChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightChanger : MonoBehaviour
+{
+    [SerializeField] private Color color;
+    [SerializeField] private float colourChangeTime;
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.GetComponent<PlayerController>())
+            StartCoroutine(ChangeGlobalLightColour());
+    }
+
+    private IEnumerator ChangeGlobalLightColour()
+    {
+        float elapsedTime = 0f;
+        Color startColour = RenderSettings.ambientLight;
+
+        while (elapsedTime < colourChangeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            RenderSettings.ambientLight = Color.Lerp(startColour, color, elapsedTime / colourChangeTime);
+            yield return null;
+        }
+        RenderSettings.ambientLight = color;
+    }
+}
