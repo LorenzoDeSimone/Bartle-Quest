b4228bb59b2c9a1c9168a8e28d2acf51fc0b4f6b
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-04-24 22:11:35 +0200

    fixed endleveldoor bug

--- Assets/Resources/Scripts/Items/ExplodingDoor.cs
+++ Assets/Resources/Scripts/Items/ExplodingDoor.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class ExplodingDoor : MonoBehaviour
 {
     [SerializeField] private ParticleSystem explosion;
-    
+    [SerializeField] private RateLevelPopupLoader rateLevelPopupLoader;
+
     public void Explode()
     {
+        if (rateLevelPopupLoader)
+            rateLevelPopupLoader.gameObject.SetActive(true);
+
+        if (GetComponent<Target>())
+            GetComponent<Target>().enabled = false;
+
         explosion.transform.parent = null;
         explosion.gameObject.SetActive(true);
         explosion.Play();
         gameObject.SetActive(false);
-
-        if(GetComponent<Target>())
-            GetComponent<Target>().enabled = false;
     }
 }
