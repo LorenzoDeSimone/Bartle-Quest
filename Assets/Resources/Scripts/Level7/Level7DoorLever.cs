b5060b202809f052861cf6badd84908512943756
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-05-01 17:06:22 +0200

    Level 7 killer path wip. Level6 reload bug found

---
+++ Assets/Resources/Scripts/Level7/Level7DoorLever.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level7DoorLever : Interactable
+{
+    [SerializeField] private Transform prisoners;
+    [SerializeField] private Transform prisonersFinalPosition;
+    [SerializeField] private GameObject player;
+    [SerializeField] private DialogueManager dialogueManager;
+    [SerializeField] private string dialogueName;
+
+    [SerializeField] private GameObject[] Enemies;
+
+    private Animation animation;
+
+    private bool leverPulled = false;
+
+    void Start()
+    {
+        animation = GetComponent<Animation>();
+    }
+
+    public override void Interact()
+    {
+        animation.Play();
+        leverPulled = true;
+        GetComponent<Target>().enabled = false;
+
+        prisoners.position = prisonersFinalPosition.position;
+        //prisoners.rotation = Quaternion.LookRotation(player.transform.position - prisoners.position, Vector3.up);
+    }
+
+    public override bool CanInteract()
+    {
+        if (leverPulled)
+        {
+            return false;
+        }
+
+        foreach (GameObject go in Enemies)
+        {
+            if (go != null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private IEnumerator StartDialogueWithNPC(Talker npcTalker)
+    {
+        yield return new WaitForSeconds(0.5f);
+        dialogueManager.InitDialogue(npcTalker);
+    }
+
+
+}
+
---
+++ Assets/Resources/Scripts/Level7/SkeletonHittable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonHittable : Hittable
+{
+    [SerializeField] private Transform patrollingGuard;
+    [SerializeField] private Transform[] guards;
+    [SerializeField] private Transform[] skeletons;
+
+    public override void UpdateHealth(int deltaHealth)
+    {
+        base.UpdateHealth(deltaHealth);
+        if (!GetComponent<EnemyStatus>().AIManager.enabled)
+            StartAttack();
+    }
+
+    private void StartAttack()
+    {
+        BartleStatistics.Instance().IncrementKiller();
+
+        foreach (Transform t in skeletons)
+            t.GetComponent<EnemyStatus>().AIManager.enabled = true;
+
+        foreach (Transform t in guards)
+            t.gameObject.SetActive(true);
+
+        Destroy(patrollingGuard.gameObject);
+    }
+
+}
